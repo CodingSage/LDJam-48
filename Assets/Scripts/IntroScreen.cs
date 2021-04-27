600856b02778165827375ae4585374be21922a61
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-04-26 21:41:05 -0400

    added intro screen

---
+++ Assets/Scripts/IntroScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScreen : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void OnMouseDown()
+    {
+        NextLevel();
+    }
+}
